3cdd467a6ab0993b0ad0183c233d3c8cf16efbd1
byronicone <byron.e.barnes@gmail.com>
2021-10-18 14:13:46 -0400

    Added react router scaffolding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        Home Page
+      </Route>
+      <Route exact path="/starred">
+        Starred Items Page
+      </Route>
+      <Route>404 Error Page</Route>
+    </Switch>
+  );
 }
 
 export default App;
