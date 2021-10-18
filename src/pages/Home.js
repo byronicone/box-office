759f35cc1df3ab12a3f65aced927370334fa793a
byronicone <byron.e.barnes@gmail.com>
2021-10-18 16:39:29 -0400

    Added search bar and button which fetch from tvmaze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 const Home = () => {
-  return <MainPageLayout>Home Page</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(console.log);
+  };
+
+  return (
+    <MainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onSubmit={onSearch}
+        value={input}
+      />
+      <button type="submit" onClick={onSearch}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
