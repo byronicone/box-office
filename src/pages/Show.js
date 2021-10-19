f737a8c5536a69268e2a932c56fbe3b21df1a90e
byronicone <byron.e.barnes@gmail.com>
2021-10-19 14:32:31 -0400

    Loading and error elements

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,9 +5,36 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(r => setShow(r));
+    let isMounted = true;
+    setIsLoading(true);
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(r => {
+        if (isMounted) {
+          setShow(r);
+          setIsLoading(false);
+        }
+      })
+      .catch(e => {
+        if (isMounted) {
+          setError(e.message);
+          setIsLoading(false);
+        }
+      });
+    return function cleanup() {
+      isMounted = false;
+    };
   }, [id]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error occurred: e</div>;
+  }
 
   return <div>{JSON.stringify(show)}</div>;
 };
