70f93695dfee25913e5ba0fdde00c1579ec71242
byronicone <byron.e.barnes@gmail.com>
2021-10-19 13:48:03 -0400

    Added styled-components to display actors and shows on a grid

--- src/components/Actor/ActorGrid.js
+++ src/components/Actor/ActorGrid.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 import ActorCard from './ActorCard';
 
 const ActorGrid = ({ data }) => {
-  return data.map(({ person }) => (
-    <ActorCard
-      id={person.id}
-      name={person.name}
-      image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
-      birthday={person.birthday}
-      deathday={person.deathday}
-      gender={person.gender}
-      country={person.country ? person.country.name : null}
-    />
-  ));
+  return (
+    <FlexGrid>
+      {data.map(({ person }) => (
+        <ActorCard
+          id={person.id}
+          name={person.name}
+          image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
+          birthday={person.birthday}
+          deathday={person.deathday}
+          gender={person.gender}
+          country={person.country ? person.country.name : null}
+        />
+      ))}
+    </FlexGrid>
+  );
 };
 
 export default ActorGrid;
