3c014cacb54bc2e29fd5aae24c1f318ef7092f43
byronicone <byron.e.barnes@gmail.com>
2021-10-21 09:57:12 -0400

    Store favorite shows in browser and show yellow star when clicked

--- src/components/Show/ShowGrid.js
+++ src/components/Show/ShowGrid.js
@@ -2,19 +2,34 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import { FlexGrid } from '../styled';
 import { IMG_PLACEHOLDER } from '../../misc/constants';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ data }) => {
+  const [starredShows, dispatchStarred] = useShows();
+
   return (
     <FlexGrid>
-      {data.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          image={show.image ? show.image.medium : IMG_PLACEHOLDER}
-          summary={show.summary}
-        />
-      ))}
+      {data.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+        const onStarClick = () => {
+          dispatchStarred({
+            type: isStarred ? 'DELETE' : 'ADD',
+            showId: show.id,
+          });
+        };
+
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            image={show.image ? show.image.medium : IMG_PLACEHOLDER}
+            summary={show.summary}
+            isStarred={isStarred}
+            onStarClick={onStarClick}
+          />
+        );
+      })}
     </FlexGrid>
   );
 };
